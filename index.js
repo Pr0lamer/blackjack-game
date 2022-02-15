153fdc5e16218c13d87b4c91ea5638b18e926bc1
Pr0lamer <andrewmakmail@gmail.com>
2022-02-15 16:38:39 +0200

    start button added + basic logic and styling

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 let minValue = 2;
 let maxValue = 11;
+let message = "";
+let isAlive = true;
+let hasBlackJack = false;
 
 let firstCard = getRandomInRange(minValue, maxValue);
-console.log(firstCard);
 let secondCard = getRandomInRange(minValue, maxValue);
-console.log(secondCard);
 let sumOfCards = firstCard + secondCard;
-console.log(sumOfCards);
+
+let startBtn = document.getElementById("start-btn");
+
+startBtn.addEventListener("click", ()=>{
+    if (sumOfCards <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sumOfCards === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! 😭";
+        isAlive = false;
+    }
+    console.log(message);
+    
+});
 
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
